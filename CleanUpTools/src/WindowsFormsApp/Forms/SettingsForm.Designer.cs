154e986255f6316dbaea6d4629d6d3b51d643b2a
corirnx <47rc91@gmail.com>
2020-12-21 12:44:53 +0100

    add

---
+++ CleanUpTools/src/WindowsFormsApp/Forms/SettingsForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WindowsFormsApp.Forms
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(646, 79);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 61);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "save settings";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(634, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "edit settings";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBack.Location = new System.Drawing.Point(646, 288);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(129, 61);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.Location = new System.Drawing.Point(12, 12);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(628, 337);
+            this.rtbContent.TabIndex = 5;
+            this.rtbContent.Text = "";
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSave);
+            this.Name = "SettingsForm";
+            this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.RichTextBox rtbContent;
+    }
+}
---
+++ CleanUpTools/src/WindowsFormsApp/Forms/SettingsForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using WindowsFormsApp.Infrastructure;
+
+namespace WindowsFormsApp.Forms
+{
+    internal partial class SettingsForm : Form
+    {
+        readonly SettingsHandler _handler;
+
+        public SettingsForm(SettingsHandler handler)
+        {
+            InitializeComponent();
+
+            _handler = handler;
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            var dir = _handler.GetDirectories();
+            // todo
+            foreach (var d in dir)
+                rtbContent.Text += $"{d.Directory.FullName};{d.Extension};{d.Recursive.ToString()};{Environment.NewLine}";
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var lines = rtbContent.Text.Split(Environment.NewLine);
+            if (!lines.Any())
+                return; // todo info
+
+            _handler.SaveSettings(rtbContent.Text);
+
+            // todo info
+
+            return;
+        }
+    }
+}
